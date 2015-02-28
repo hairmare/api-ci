6ac01df1b32f0b46cf137c2c430247f5fd659c7e
Lucas Bickel <hairmare@purplehaze.ch>
2015-02-28 04:24:16 +0100

    no backups during worker push

--- push-worker.sh
+++ push-worker.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-sed -i -e 's/"WEB_SERVER": "nginx",/"WEB_SERVER": "none",/' .bp-config/options.json
+sed -i '' -e 's/"WEB_SERVER": "nginx",/"WEB_SERVER": "none",/' .bp-config/options.json
 cf push api-ci-worker
-sed -i -e 's/"WEB_SERVER": "none",/"WEB_SERVER": "nginx",/' .bp-config/options.json
-
+sed -i '' -e 's/"WEB_SERVER": "none",/"WEB_SERVER": "nginx",/' .bp-config/options.json
